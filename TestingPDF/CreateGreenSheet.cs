323ab8e64ffe0253e1ac52b72debef4adfc6a783
Mike Richardson <mike@completedevsolutions.com>
2023-01-06 06:02:47 -0600

    Broke Font Resolver into it's own class

--- TestingPDF/CreateGreenSheet.cs
+++ TestingPDF/CreateGreenSheet.cs
@@ -10,19 +10,11 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
-using PdfSharp.Drawing.Layout;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using Azure.Storage.Blobs;
-using System.Text;
-using Azure;
-using System.Runtime.InteropServices;
 using System;
-using PdfSharp.Fonts;
-using System.Reflection;
-using static System.Net.Mime.MediaTypeNames;
-using System.Drawing;
-using Azure.Security.KeyVault.Secrets;
+
 
 namespace GreenSheetCreator
 {
@@ -160,115 +152,7 @@ namespace GreenSheetCreator
         }
 
 
-    }
-
-
-    class MyFontResolver : IFontResolver
-    {
-
-        public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
-        {
-            // Ignore case of font names.
-            var name = familyName.ToLower().TrimEnd('#');
-
-            // Deal with the fonts we know.
-            switch (name)
-            {
-                case "fre3of9x":
-                    return new FontResolverInfo("fre3of9x#");
-                case "arial":
-                    if (isBold)
-                    {
-                        return new FontResolverInfo("Arial#b");
-                    }
-                    return new FontResolverInfo("Arial#");
-            }
-
-            // We pass all other font requests to the default handler.
-            // When running on a web server without sufficient permission, you can return a default font at this stage.
-            return PlatformFontResolver.ResolveTypeface(familyName, isBold, isItalic);
-        }
-
-        /// <summary>
-        /// Return the font data for the fonts.
-        /// </summary>
-        public byte[] GetFont(string faceName)
-        {
-            switch (faceName)
-            {
-                case "fre3of9x#":
-                    return FontHelper.fre3of9x;
-                case "Arial#":
-                    return FontHelper.Arial;
-
-                case "Arial#b":
-                    return FontHelper.arialbd;
-
-            }
-
-            return null;
-        }
-
-
-        internal static MyFontResolver OurGlobalFontResolver = null;
-
-        /// <summary>
-        /// Ensure the font resolver is only applied once (or an exception is thrown)
-        /// </summary>
-        internal static void Apply()
-        {
-            if (OurGlobalFontResolver == null || GlobalFontSettings.FontResolver == null)
-            {
-                if (OurGlobalFontResolver == null)
-                    OurGlobalFontResolver = new MyFontResolver();
-
-                GlobalFontSettings.FontResolver = OurGlobalFontResolver;
-            }
-        }
-    }
-
-
-    /// <summary>
-    /// Helper class that reads font data from embedded resources.
-    /// </summary>
-    public static class FontHelper
-    {
-        public static byte[] fre3of9x
-        {
-            get { return LoadFontData("GreenSheetCreator.Fonts.fre3of9x.ttf"); }
-        }
-
-        public static byte[] Arial
-        {
-            get { return LoadFontData("GreenSheetCreator.Fonts.Arial.ttf"); }
-        }
-        public static byte[] arialbd
-        {
-            get { return LoadFontData("GreenSheetCreator.Fonts.arialbd.ttf"); }
-        }
-
-        /// <summary>
-        /// Returns the specified font from an embedded resource.
-        /// </summary>
-        static byte[] LoadFontData(string name)
-        {
-            var assembly = Assembly.GetExecutingAssembly();
-
-            // Test code to find the names of embedded fonts
-            //var ourResources = assembly.GetManifestResourceNames();
-
-            using (Stream stream = assembly.GetManifestResourceStream(name))
-            {
-                if (stream == null)
-                    throw new ArgumentException("No resource with name " + name);
-
-                int count = (int)stream.Length;
-                byte[] data = new byte[count];
-                stream.Read(data, 0, count);
-                return data;
-            }
-        }
-    }
+    }   
 
 }
 
